63a5e4d58c31f97b84194864e0db77dc60b0a966
canofranck <cano.franck77@gmail.com>
2024-07-26 12:23:52 +0200

    etape 1  fini

---
+++ lettings/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from lettings import views
+
+app_name = 'lettings'
+urlpatterns = [
+    path('', views.index, name='lettings_index'),
+    path('<int:letting_id>/', views.letting, name='letting'),
+]
