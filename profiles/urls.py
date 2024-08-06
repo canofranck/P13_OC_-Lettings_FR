7673621545fea9b96240ca7ec7525362e9138909
canofranck <cano.franck77@gmail.com>
2024-08-06 13:20:26 +0200

    debut etape2

---
+++ profiles/urls.py
@@ -0,0 +1,10 @@
+# profiles/urls.py
+from django.urls import path
+from profiles import views
+
+app_name = "profiles"
+
+urlpatterns = [
+    path("profiles/", views.index, name="profiles_index"),
+    path("profiles/<str:username>/", views.profile, name="profile"),
+]
