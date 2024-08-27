ac78e03f283b4aecd80105532a625d735aa1e1a1
canofranck <cano.franck77@gmail.com>
2024-08-27 16:08:38 +0200

    update render 20

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # STATICFILES_DIRS = [BASE_DIR / "static",]
+    STATICFILES_DIRS = [BASE_DIR / "staticfiles",]
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
