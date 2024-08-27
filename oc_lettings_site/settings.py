39d9d28f560fc40e47d3aa362b6f21c30fe9c403
canofranck <cano.franck77@gmail.com>
2024-08-27 15:27:25 +0200

    update render 17

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -115,10 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR / "static",]
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATIC_ROOT = BASE_DIR / "staticfiles"
+    
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
