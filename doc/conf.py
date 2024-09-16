0dad37cb206fc6373b924c7f91918a543d5d4b47
canofranck <cano.franck77@gmail.com>
2024-09-16 12:35:54 +0200

    update doc

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,3 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'nature'
-html_static_path = ["_static"]
-
-locale_dirs = ["locale/"]  # path is example but recommended.
-gettext_compact = False
